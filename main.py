8ec49daad8177f25f85394f88a7a3ef1c22c403c
outfielder <67472522+outfielder@users.noreply.github.com>
2021-06-28 21:12:45 +0100

    complete the image manipulation and print functionality

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from pdf2image import convert_from_path
-pages = convert_from_path('pdf_file', 500)
+import cv2
+import os
 
-# Saving pages in jpeg format
+pdf_labels = [a for a in os.listdir() if a.endswith(".pdf")]
 
-for page in pages:
-    page.save('out.jpg', 'JPEG')
+pdf_labels.sort()
+
+for label_number, label_name in enumerate(pdf_labels):
+    pages = convert_from_path(label_name, 500)
+
+    # Saving pages in jpeg format
+
+    for page in pages:
+        page.save(f'label_{label_number}.jpg', 'JPEG')
+
+    img = cv2.imread(f'label_{label_number}.jpg')
+    crop_img = img[2676:4719, 243:3155]
+    rotated_img = cv2.rotate(crop_img, rotateCode=cv2.ROTATE_90_CLOCKWISE)
+    cv2.imwrite(f'label_{label_number}.jpg', rotated_img)
+    os.system(f'i_view64 {f"label_{label_number}.jpg"} /print')
