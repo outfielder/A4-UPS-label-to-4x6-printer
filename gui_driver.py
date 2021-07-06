d6cf3bc7c133167f36898f353c3c324bb3bfc0ee
outfielder <67472522+outfielder@users.noreply.github.com>
2021-07-06 23:30:34 +0100

    add file chooser button and gui window

---
+++ gui_driver.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import filedialog
+
+
+def UploadAction(event=None):
+    filename = filedialog.askopenfilename()
+    print('Selected:', filename)
+
+
+root = tk.Tk()
+button = tk.Button(root, text='Open', command=UploadAction)
+button.pack()
+
+root.mainloop()
